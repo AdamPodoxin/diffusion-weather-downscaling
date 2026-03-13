706aaee4016086486dabc0409dc999e5c5cc3f56
Adam Podoxin <adam.podoxin@gmail.com>
2026-03-13 00:03:01 -0700

    Switched to L1 loss, as described in paper

--- train-vqvae.py
+++ train-vqvae.py
@@ -17,8 +17,12 @@ TRAIN_PATH = DATA_PATH / "train.zarr"
 MODELS_PATH = Path("models")
 VQVAE_PATH = MODELS_PATH / "vqvae"
 
-BATCH_SIZE = 128
-NUM_EPOCHS = 100
+# Batch size set to whatever GPU VRAM can handle
+BATCH_SIZE = 96
+
+# Loss value seems to reach minimum after only 5 epochs,
+# then begins rising again, possible overfitting.
+NUM_EPOCHS = 5
 
 WIND_DIR_AND_PRESSURE_CHANNELS = \
 [
@@ -59,39 +63,6 @@ def generate_batches(data: xr.DataArray, batch_size=32):
         yield batch_tensor
 
 
-def train_vqvae(vqvae: VQModel, low_res_inputs: xr.DataArray, device: str):
-    optimizer = torch.optim.Adam(vqvae.parameters(), lr=2e-4)
-    scheduler = CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS)
-
-    num_batches = low_res_inputs.sizes["sample"] // BATCH_SIZE
-
-    vqvae.train()
-
-    for epoch in range(NUM_EPOCHS):
-        print("\n", "Epoch", epoch + 1)
-        
-        batch_generator = generate_batches(low_res_inputs, batch_size=BATCH_SIZE)
-        for X in tqdm(batch_generator, total=num_batches):
-            X = X.to(device)
-            X_normalized, _, _ = normalize_across_channels(X)
-
-            output: DecoderOutput = vqvae(X)
-            output_sample = output.sample
-            output_sample_normalized, _, _ = normalize_across_channels(output_sample)
-
-            loss = torch.nn.functional.mse_loss(output_sample_normalized, X_normalized)
-            loss.backward()
-
-            optimizer.step()
-            optimizer.zero_grad()
-        
-        print(f"Loss: {loss:.2e}")
-        
-        scheduler.step()
-    
-    torch.save(vqvae.state_dict(), VQVAE_PATH / "vqvae-trained.pt")
-
-
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Running on", device)
@@ -102,7 +73,36 @@ if __name__ == "__main__":
     vqvae: VQModel = pipeline.vqvae
 
     ds_train = xr.open_zarr(TRAIN_PATH)
-    X_lr = ds_train["X_lr"]
-    X_lr_wind_dir_and_pressure = X_lr.sel(channel=WIND_DIR_AND_PRESSURE_CHANNELS)
+    X_lr_train = ds_train["X_lr"]
+    X_lr_train_subset = X_lr_train.sel(channel=WIND_DIR_AND_PRESSURE_CHANNELS)
+    
+    optimizer = torch.optim.Adam(vqvae.parameters(), lr=2e-4)
+    scheduler = CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS)
 
-    train_vqvae(vqvae, X_lr_wind_dir_and_pressure, device)
+    num_batches = X_lr_train_subset.sizes["sample"] // BATCH_SIZE
+
+    vqvae.train()
+
+    for epoch in range(NUM_EPOCHS):
+        print("\n", "Epoch", epoch + 1)
+        
+        batch_generator = generate_batches(X_lr_train_subset, batch_size=BATCH_SIZE)
+        for X in tqdm(batch_generator, total=num_batches):
+            X = X.to(device)
+            X_normalized, _, _ = normalize_across_channels(X)
+
+            output: DecoderOutput = vqvae(X)
+            output_sample = output.sample
+            output_sample_normalized, _, _ = normalize_across_channels(output_sample)
+
+            loss = torch.nn.functional.l1_loss(output_sample_normalized, X_normalized)
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+        
+        print(f"Loss: {loss:.2f}")
+        
+        scheduler.step()
+            
+    torch.save(vqvae.state_dict(), VQVAE_PATH / "vqvae-trained.pt")
