6ed74ff253448e63f0accd5d89f2660e94e015e5
Adam Podoxin <adam.podoxin@gmail.com>
2026-03-31 20:43:34 -0700

    Tweak unet training code

--- train-vanilla-unet.py
+++ train-vanilla-unet.py
@@ -8,6 +8,7 @@ import torch
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
 import xarray as xr
+import pandas as pd
 from tqdm import tqdm
 
 import math
@@ -15,7 +16,6 @@ import math
 from utils import (
     load_model_state_dict,
     generate_batches,
-    # normalize_across_channels,
     save_checkpoint,
     get_4channel_unet,
     get_lora_unet,
@@ -24,24 +24,28 @@ from utils import (
 
 
 # For CSIL
-DATA_PATH = Path("/usr/shared/CMPT/scratch/alp11/data/cmpt420/project")
-# DATA_PATH = Path("data")
+# DATA_PATH = Path("/usr/shared/CMPT/scratch/alp11/data/cmpt420/project")
+DATA_PATH = Path("data")
 TRAIN_PATH = DATA_PATH / "train.zarr"
 VAL_PATH = DATA_PATH / "val.zarr"
 
 MODELS_DIR = Path("models")
 VQVAE_PATH = MODELS_DIR / "vqvae-trained" / "vqvae-trained.pt"
 UNET_DIR = MODELS_DIR / "unet-trained-vanilla"
+LOSSES_DIR = UNET_DIR / "losses"
 
 # Set to whatever GPU VRAM can handle, but must be factor of 
 # number of training samples AND number of validation samples.
-BATCH_SIZE = 100
+BATCH_SIZE = 50
 
-NUM_EPOCHS = 20
+NUM_EPOCHS = 10
 SAVE_EVERY_EPOCH = False
 
 
 if __name__ == "__main__":
+    UNET_DIR.mkdir(exist_ok=True)
+    LOSSES_DIR.mkdir(exist_ok=True)
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Running on", device)
 
@@ -63,6 +67,51 @@ if __name__ == "__main__":
     X_lr_train = ds_train["X_lr"]
     X_hr_train = ds_train["X_hr"]
 
+    # Calculate mean and std per channel for normalization
+    print("Calculating LR means and stds")
+    with torch.no_grad():
+        # Not completely ideal because loading entire dataset into VRAM,
+        # but it should fit for our data so it's fine for now. 
+        temp_train_tensor = torch.from_numpy(X_lr_train.to_numpy()).to(device)
+        height = temp_train_tensor.shape[2]
+        width = temp_train_tensor.shape[3]
+        
+        # Calculate per each channel (dim 1), and
+        # expand to size of batch tensor. 
+        lr_means = temp_train_tensor \
+                .mean(dim=(0, 2, 3)) \
+                .view(1, 4, 1, 1) \
+                .expand(BATCH_SIZE, 4, height, width)
+        
+        lr_stds = temp_train_tensor \
+                .std(dim=(0, 2, 3)) \
+                .view(1, 4, 1, 1) \
+                .expand(BATCH_SIZE, 4, height, width)
+
+    # Calculate mean and std per channel for normalization
+    print("Calculating HR means and stds")
+    with torch.no_grad():
+        # Not completely ideal because loading entire dataset into VRAM,
+        # but it should fit for our data so it's fine for now. 
+        temp_train_tensor = torch.from_numpy(X_hr_train.to_numpy()).to(device)
+        height = temp_train_tensor.shape[2]
+        width = temp_train_tensor.shape[3]
+        
+        # Calculate per each channel (dim 1), and
+        # expand to size of batch tensor. 
+        hr_means = temp_train_tensor \
+                .mean(dim=(0, 2, 3)) \
+                .view(1, 4, 1, 1) \
+                .expand(BATCH_SIZE, 4, height, width)
+        
+        hr_stds = temp_train_tensor \
+                .std(dim=(0, 2, 3)) \
+                .view(1, 4, 1, 1) \
+                .expand(BATCH_SIZE, 4, height, width)
+
+    del temp_train_tensor
+    torch.cuda.empty_cache()
+
     ds_val = xr.open_zarr(VAL_PATH)
     X_lr_val = ds_val["X_lr"]
     X_hr_val = ds_val["X_hr"]
@@ -81,12 +130,13 @@ if __name__ == "__main__":
 
     def loop_logic(X: torch.Tensor, Y: torch.Tensor):
         X = X.to(device)
-        X_normalized, _, _ = normalize_across_channels(X)
+        X_normalized = (X - lr_means) / lr_stds
 
         Y = Y.to(device)
-        Y_normalized, _, _ = normalize_across_channels(Y)
+        Y_normalized = (Y - hr_means) / hr_stds
 
         with torch.no_grad():
+            # Use VQVAE to compute "ideal" latents
             latents = vqvae.encode(Y_normalized).latents
         
         noise = torch.randn_like(latents)
@@ -98,9 +148,11 @@ if __name__ == "__main__":
             device=device
         ).long()
 
+        # Add noise to "ideal" latents to simulate various
+        # levels that UNet will encounter during inference. 
         noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-        unet_input = torch.cat([noisy_latents, X_normalized], dim=1)
 
+        unet_input = torch.cat([noisy_latents, X_normalized], dim=1)
         unet_output: UNet2DOutput = unet(unet_input, timesteps)
         predicted_noise = unet_output.sample
 
@@ -108,6 +160,9 @@ if __name__ == "__main__":
 
         return loss
 
+
+    train_losses = [math.inf for _ in range(NUM_EPOCHS)]
+    val_losses = [math.inf for _ in range(NUM_EPOCHS)]
 
     for epoch in range(NUM_EPOCHS):
         unet.train()
@@ -130,6 +185,7 @@ if __name__ == "__main__":
         unet.eval()
 
         avg_train_loss /= num_batches
+        train_losses[epoch] = avg_train_loss
 
         num_val_batches = X_lr_val.sizes["sample"] // BATCH_SIZE
         avg_val_loss = 0
@@ -144,6 +200,7 @@ if __name__ == "__main__":
                 avg_val_loss += loss_val.item()
 
         avg_val_loss /= num_val_batches
+        val_losses[epoch] = avg_val_loss
 
         print(f"Average training loss: {avg_train_loss:.4f}")
         print(f"Average validation loss: {avg_val_loss:.4f}")
@@ -162,6 +219,13 @@ if __name__ == "__main__":
                 path=UNET_DIR / f"unet-trained-vanilla-{epoch}.pt"
             )
 
+        loss_df = pd.DataFrame({
+            "epoch": [epoch],
+            "train loss": [avg_train_loss], 
+            "validation loss": [avg_val_loss]
+        })
+        loss_df.to_csv(LOSSES_DIR / f"epoch-{epoch:03}.csv", index=False)
+
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
             best_epoch = epoch
@@ -177,3 +241,12 @@ if __name__ == "__main__":
             )
     
     print("Epoch", best_epoch, f"had lowest validation loss {best_val_loss:.4f}")
+
+    losses_df = pd.DataFrame(
+        data={
+            "epoch": [epoch for epoch in range(NUM_EPOCHS)],
+            "train loss": train_losses,
+            "validation loss": val_losses,
+        }
+    )
+    losses_df.to_csv(LOSSES_DIR / "losses.csv", index=False)
