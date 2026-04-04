9e0686a2722fa6a42b68e8ed7088a82dfcfac68c
Adam Podoxin <adam.podoxin@gmail.com>
2026-04-03 20:19:13 -0700

    Update plot pipeline outputs

--- plot-pipeline-outputs.py
+++ plot-pipeline-outputs.py
@@ -1,38 +1,107 @@
-import sys
+import argparse
 from pathlib import Path
 import xarray as xr
-from weather_downscaling_pipeline import WeatherLDMSuperResolutionPipeline
 import torch
-import torchvision
+from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
 
-DATA_PATH = Path("data")
-TEST_PATH = DATA_PATH / "test.zarr"
+CHANNEL_ORDER = ["10m_u_component_of_wind", "10m_v_component_of_wind", "2m_temperature", "mean_sea_level_pressure"]
+COLOR_MAP = {
+    "10m_u_component_of_wind": "winter",
+    "10m_v_component_of_wind": "winter",
+    "2m_temperature": "autumn",
+    "mean_sea_level_pressure": "cool",
+}
 
-SAMPLE_INDEX = 0
 
-NUM_INFERENCE_STEPS = 100
+def parse_args():
+    p = argparse.ArgumentParser("Visualize pipeline outputs")
+
+    p.add_argument(
+        "--input",
+        type=Path,
+        default=Path("evaluation/test_outputs/vanilla/normalized.pt"),
+        help="Normalized test outputs file",
+    )
+    p.add_argument(
+        "--output",
+        type=Path,
+        default=Path("plots/pipeline_outputs/vanilla.png"),
+        help="Output image file",
+    )
+    p.add_argument(
+        "--test-file",
+        type=Path,
+        default=Path("data/test.zarr"),
+        help="File containing ground truth HR output",
+    )
+    p.add_argument(
+        "--pipeline",
+        type=str,
+        default="Vanilla",
+        help="Pipeline name",
+    )
+    p.add_argument(
+        "--sample",
+        type=int,
+        default=0,
+        help="Sample index",
+    )
+
+    return p.parse_args()
 
 
 if __name__ == "__main__":
-    ds_test = xr.open_zarr(TEST_PATH)
-    X_lr_test = ds_test["X_lr"]
-    X_hr_test = ds_test["X_hr"]
+    args = parse_args()
 
-    X_lr_test = X_lr_test.isel(sample=(slice(SAMPLE_INDEX, SAMPLE_INDEX + 1)))
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    pipeline = WeatherLDMSuperResolutionPipeline(
-        vqvae_path="./models/vqvae-trained-psd/vqvae-trained-psd.pt",
-        unet_path="./models/unet-trained-psd/unet-trained-psd.pt",
-    )
+    ds_test = xr.open_zarr(args.test_file)
+    X_hr = ds_test["X_hr"]
 
-    _, Y = pipeline(X_lr_test, NUM_INFERENCE_STEPS)
-
-    fig, axs = plt.subplots(2, 4, figsize=(10, 5))
+    target = torch.from_numpy(X_hr.values).to(device)
+    
+    num_samples = target.shape[0]
+    height = target.shape[2]
+    width = target.shape[3]
+    
     with torch.no_grad():
-        for channel in range(4):
-            axs[0, channel].imshow(torchvision.utils.make_grid(Y[0].cpu())[channel], cmap='Greys')
-            axs[1, channel].imshow(torchvision.utils.make_grid(torch.from_numpy(X_hr_test[0].values))[channel], cmap='Greys')
+        target_means = target \
+                    .mean(dim=(0, 2, 3)) \
+                    .view(1, 4, 1, 1) \
+                    .expand(num_samples, 4, height, width)
+        target_stds = target \
+                    .std(dim=(0, 2, 3)) \
+                    .view(1, 4, 1, 1) \
+                    .expand(num_samples, 4, height, width)
 
-    fig.savefig("pipeline-outputs-psd.png")
+        target_normalized = (target - target_means) / target_stds
+
+    predicted_normalized: torch.Tensor = torch.load(args.input)
+
+    sample_index = int(args.sample)
+
+    target_grid = make_grid(target_normalized.cpu()[sample_index])
+    predicted_grid = make_grid(predicted_normalized.cpu()[sample_index])
+
+    fig, axs = plt.subplots(2, 4, figsize=(16, 8))
+
+    fig.suptitle(f"{args.pipeline} pipeline outputs", fontsize=20)
+
+    axs[0, 0].set_ylabel("Predicted", rotation=0, labelpad=34, fontsize=14)
+    axs[1, 0].set_ylabel("Target", rotation=0, labelpad=34, fontsize=14)
+
+    for channel_index, channel in enumerate(CHANNEL_ORDER):
+        predicted_ax: Axes = axs[0, channel_index]
+        target_ax: Axes = axs[1, channel_index]
+
+        predicted_ax.set_title(channel)
+
+        cmap = COLOR_MAP[channel]
+
+        predicted_ax.imshow(predicted_grid[channel_index], cmap=cmap)
+        target_ax.imshow(target_grid[channel_index], cmap=cmap)
+
+    fig.savefig(args.output, bbox_inches="tight")
