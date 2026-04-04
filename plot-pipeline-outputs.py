03b6bedf569fa7bd3d92471bfeb2a4d5b8b7744b
Adam Podoxin <adam.podoxin@gmail.com>
2026-04-03 21:26:25 -0700

    Include interpolated LR in pipeline visualizations

--- plot-pipeline-outputs.py
+++ plot-pipeline-outputs.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 import xarray as xr
 import torch
+from torch.nn.functional import interpolate
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
@@ -60,47 +61,64 @@ if __name__ == "__main__":
 
     ds_test = xr.open_zarr(args.test_file)
     X_hr = ds_test["X_hr"]
+    X_lr = ds_test["X_lr"]
 
     target = torch.from_numpy(X_hr.values).to(device)
+    lr = torch.from_numpy(X_lr.values).to(device)
     
     num_samples = target.shape[0]
     height = target.shape[2]
     width = target.shape[3]
     
     with torch.no_grad():
-        target_means = target \
+        means_base = target \
                     .mean(dim=(0, 2, 3)) \
-                    .view(1, 4, 1, 1) \
-                    .expand(num_samples, 4, height, width)
-        target_stds = target \
+                    .view(1, 4, 1, 1)
+        stds_base = target \
                     .std(dim=(0, 2, 3)) \
-                    .view(1, 4, 1, 1) \
-                    .expand(num_samples, 4, height, width)
+                    .view(1, 4, 1, 1)
+
+        target_means = means_base.expand(num_samples, 4, height, width)
+        target_stds = stds_base.expand(num_samples, 4, height, width)
 
         target_normalized = (target - target_means) / target_stds
+        
+    height_lr = lr.shape[2]
+    width_lr = lr.shape[3]
+    
+    with torch.no_grad():
+        lr_means = means_base.expand(num_samples, 4, height_lr, width_lr)
+        lr_stds = stds_base.expand(num_samples, 4, height_lr, width_lr)
+
+        lr_normalized = (lr - lr_means) / lr_stds
+        lr_interpolated = interpolate(lr_normalized, scale_factor=4)
 
     predicted_normalized: torch.Tensor = torch.load(args.input)
 
     sample_index = int(args.sample)
 
     target_grid = make_grid(target_normalized.cpu()[sample_index])
+    lr_grid = make_grid(lr_interpolated.cpu()[sample_index])
     predicted_grid = make_grid(predicted_normalized.cpu()[sample_index])
 
-    fig, axs = plt.subplots(2, 4, figsize=(16, 8))
+    fig, axs = plt.subplots(3, 4, figsize=(16, 12))
 
     fig.suptitle(f"{args.pipeline} pipeline outputs", fontsize=20)
 
-    axs[0, 0].set_ylabel("Predicted", rotation=0, labelpad=34, fontsize=14)
-    axs[1, 0].set_ylabel("Target", rotation=0, labelpad=34, fontsize=14)
+    axs[0, 0].set_ylabel("LR\ninterpolated", rotation=0, labelpad=40, fontsize=14)
+    axs[1, 0].set_ylabel("Predicted", rotation=0, labelpad=40, fontsize=14)
+    axs[2, 0].set_ylabel("Target", rotation=0, labelpad=40, fontsize=14)
 
     for channel_index, channel in enumerate(CHANNEL_ORDER):
-        predicted_ax: Axes = axs[0, channel_index]
-        target_ax: Axes = axs[1, channel_index]
+        lr_ax: Axes = axs[0, channel_index]
+        predicted_ax: Axes = axs[1, channel_index]
+        target_ax: Axes = axs[2, channel_index]
 
         predicted_ax.set_title(channel)
 
         cmap = COLOR_MAP[channel]
 
+        lr_ax.imshow(lr_grid[channel_index], cmap=cmap)
         predicted_ax.imshow(predicted_grid[channel_index], cmap=cmap)
         target_ax.imshow(target_grid[channel_index], cmap=cmap)
 
